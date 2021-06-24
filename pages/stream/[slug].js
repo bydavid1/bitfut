ea92a3a5ab0a1a0a4b00c32eddbfe3ec70b168c9
bydavid1 <byronjimenez9911@gmail.com>
2021-06-24 12:30:22 -0600

    Share buttons added
    Open graph properties added

--- pages/stream/[slug].js
+++ pages/stream/[slug].js
@@ -3,16 +3,27 @@ import Channel from '../../models/Channel'
 import Head from 'next/head'
 import { useRef } from 'react'
 
+import { FacebookShareButton,
+        FacebookIcon, 
+        TwitterShareButton,
+        TwitterIcon,
+        WhatsappShareButton,
+        WhatsappIcon,
+        TelegramShareButton,
+        TelegramIcon,} from 'next-share'
+
 export default function Stream({currentChannel}) {
 
     let frameElement = useRef(null)
+    let currentPath = ''
+    if (typeof window !== 'undefined') {
+        currentPath = window.location.href;
+     }
 
     function blockScripts() {
         let m = document.createElement("meta");
         m.httpEquiv = "content-security-policy";
         m.content = "script-src 'self' 'unsafe-inline';";
-
-        console.log(frameElement)
     }
 
     return (
@@ -20,6 +31,10 @@ export default function Stream({currentChannel}) {
             <Head>
                 <title>{currentChannel.name}</title>
                 <meta http-equiv="Content-Security-Policy" content="script-src 'self' *.telerium.club;"/>
+                <meta property="og:title" content={ `Bitfutbol - ${currentChannel.name}` }  />
+                <meta property="og:description" content={`Mira ${currentChannel.name} online y totalmente gratis`} />
+                <meta property="og:image" content="/images/static/meta-image.jpg"/>
+                <meta property="og:url" content={currentPath}/>
             </Head>
             <Layout>
                 <div className="container mt-5 py-5">
@@ -35,12 +50,35 @@ export default function Stream({currentChannel}) {
                                 height="450"
                                 frameBorder="0" 
                                 scrolling="no" 
-                                allowFullScreen="true"
+                                allowFullScreen={true}
                                 onLoad={blockScripts}/>
                             </div>
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item">Compartir</li>
-                                <li className="list-group-item">Facebook</li>
+                                <li className="list-group-item">
+                                <FacebookShareButton
+                                    url={currentPath}
+                                    hashtag={'#bitfutbol'}
+                                    className="pr-2">
+                                    <FacebookIcon size={32} round />
+                                </FacebookShareButton>
+                                <TwitterShareButton
+                                    url={currentPath}
+                                    hashtag={'#bitfutbol'}
+                                    className="pr-2">
+                                    <TwitterIcon size={32} round />
+                                </TwitterShareButton>
+                                <WhatsappShareButton
+                                    url={currentPath}
+                                    className="pr-2">
+                                    <WhatsappIcon size={32} round />
+                                </WhatsappShareButton>
+                                <TelegramShareButton
+                                    url={currentPath}
+                                    className="pr-2">
+                                    <TelegramIcon size={32} round />
+                                </TelegramShareButton>
+                                </li>
                                 <li className="list-group-item">Twitter</li>
                             </ul>
                         </div>
@@ -54,7 +92,6 @@ export default function Stream({currentChannel}) {
 export async function getServerSideProps(context) {
     const { slug } = context.query
     let currentChannel = {}
-
     let results = await Channel.findOne({slug: slug}).select('source name')
     currentChannel = JSON.parse(JSON.stringify(results))
 
